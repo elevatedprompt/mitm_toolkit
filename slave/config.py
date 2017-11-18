2276517133cb90ad747d11c544ca1b3215140594
ichintu <ichintu@gmail.com>
2017-11-18 00:13:13 -0500

    Updated dummy configuration

--- slave/config.py
+++ slave/config.py
@@ -1,8 +1,8 @@
 # Update with unique ID
-CONSUMER_KEY = "DEMO-b9531eca-9db6-421e-9145-933ff4ff0364"
+# CONSUMER_KEY = "DEMO-b9531eca-9db6-421e-9145-933ff4ff0364"
 # Update with proper URI for websocket server
-WS_SERVER = "ws://127.0.0.1:8080/"  # "ws://r2d2.byteseclabs.com:8000/"
+WS_SERVER = "ws://127.0.0.1:8080/"
 # Update the source & destination IP to ignore websocket server traffic
-WS_SERVER_IP = "127.0.0.1"  # "75.127.10.66"
+# WS_SERVER_IP = "127.0.0.1"
 # Update to match the monitor interface
 # MON_IFACE = "enp0s3"
